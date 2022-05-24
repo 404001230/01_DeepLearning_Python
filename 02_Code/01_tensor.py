139120d44c80e25629e8cefe650fc3e55abb5369
huaweifan <404001230@qq.com>
2022-05-24 18:29:52 +0800

    tensor

---
+++ 02_Code/01_tensor.py
@@ -0,0 +1,12 @@
+'''
+Author: huaweifan 404001230@qq.com
+Date: 2022-05-24 18:26:48
+LastEditors: huaweifan 404001230@qq.com
+LastEditTime: 2022-05-24 18:27:09
+FilePath: /01_DeepLearning/02_Code/01_tensor.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import tensorflow as tf
+
+print("aaaa")
+ 
